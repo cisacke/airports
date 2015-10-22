aed8ce90cfdc108c2be636c38a2bf94eecd5f133
cisacke <ci382@nyu.edu>
2015-10-22 11:07:42 -0400

    Attempt to Fix GMaps Production Bug#2

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -10,10 +10,9 @@
 // Read Sprockets README (https://github.com/rails/sprockets#sprockets-directives) for details
 // about supported directives.
 //
-//= require_self
-//= require_tree .
 //= require gmaps/google
 //= require jquery
 //= require jquery_ujs
 //= require jquery-ui
 //= require underscore
+//= require_tree .

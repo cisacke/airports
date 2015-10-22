4c93b5fd3f969519349681f752ae9b17683cf66f
cisacke <ci382@nyu.edu>
2015-10-22 11:05:21 -0400

    Attempt to Fix GMaps Production Bug

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -12,9 +12,8 @@
 //
 //= require_self
 //= require_tree .
+//= require gmaps/google
 //= require jquery
 //= require jquery_ujs
 //= require jquery-ui
 //= require underscore
-//= require gmaps/google
-//= require airports

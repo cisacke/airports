cbde786cdd425918c9e6e982198b716010a8fccd
cisacke <ci382@nyu.edu>
2015-10-21 21:26:55 -0400

    Add Google Maps, change to nms

--- app/controllers/airports_controller.rb
+++ app/controllers/airports_controller.rb
@@ -2,15 +2,19 @@ class AirportsController < ActionController::Base
   require 'rubygems'
   require 'geokit'
   include Geokit::Geocoders
+  Geokit::default_units = :nms  
 
   def index
-    @airports = Airport.pluck(:name)
+    @airports = []
+    Airport.pluck(:name).zip(Airport.pluck(:code)).each do |airport|
+      @airports << airport.join(" (") + ")"
+    end
     render :index
   end
 
   def distance
-    airport_a = Airport.where(name: params[:airport_a])[0]
-    airport_b = Airport.where(name: params[:airport_b])[0]
+    airport_a = Airport.where(name: params[:airport_a].split(" (")[0])[0]
+    airport_b = Airport.where(name: params[:airport_b].split(" (")[0])[0]
 
     if airport_b && airport_a
       @distance = distance_from(airport_a, airport_b)
@@ -18,6 +22,18 @@ class AirportsController < ActionController::Base
     else
       render json: "faulty"
     end
+  end
+
+  def map
+    airport_a = Airport.where(name: params[:airport_a].split(" (")[0])[0]
+    airport_b = Airport.where(name: params[:airport_b].split(" (")[0])[0]
+
+    @airports = [airport_a] + [airport_b]
+    @hash = Gmaps4rails.build_markers(@airports) do |airport, marker|
+      marker.lat airport.latitude
+      marker.lng airport.longitude
+    end
+    render json: @hash
   end
 
   private
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
   resources :airports, only: :index
   get 'airports/distance', to: "airports#distance"
+  get 'airports/map', to: 'airports#map'
 end
